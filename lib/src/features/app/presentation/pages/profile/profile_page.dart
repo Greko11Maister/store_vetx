302f861babe1674d4e86ca0981f2b86c2eaab017
Gregory Iscala <greko11maister@gmail.com>
2021-05-26 10:03:49 -0500

    show data of account inside profile page

--- lib/src/features/app/presentation/pages/profile/profile_page.dart
+++ lib/src/features/app/presentation/pages/profile/profile_page.dart
@@ -1,30 +1,36 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
+import 'package:store_vtex/src/features/app/presentation/controllers/account_controller.dart';
 
 import 'create_account_page.dart';
 
-class  ProfilePage extends StatelessWidget {
- static const routeName = '/profile/page';
-  const  ProfilePage({Key key}) : super(key: key);
+class ProfilePage extends StatelessWidget {
+  static const routeName = '/profile/page';
+  AccountController _controller;
+
+  ProfilePage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    _controller = Get.find();
+    return Scaffold(
       appBar: AppBar(),
-      body: Column(
+      body: GetBuilder<AccountController>(builder: (_)=> Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
-            padding: EdgeInsets.only(top: 80, left: 20.0,bottom: 15.0),
+            padding: EdgeInsets.only(top: 80, left: 20.0, bottom: 15.0),
             child: Row(
               children: [
                 Container(
                   child: CircleAvatar(
-                    backgroundColor:Colors.grey,
-                    child: Icon(Icons.account_circle_outlined, size: 120, color: Colors.black,),
+                    backgroundColor: Colors.grey,
+                    child: Icon(
+                      Icons.account_circle_outlined,
+                      size: 120,
+                      color: Colors.black,
+                    ),
                   ),
                   constraints: BoxConstraints(
                     minHeight: 120,
@@ -39,15 +45,24 @@ class  ProfilePage extends StatelessWidget {
                   children: [
                     Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: Text('Nombre: Juan Carlos Rojas', style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.w500),),
+                      child: Text(
+                        'Nombre: ${_.user.email}',
+                        style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.w500),
+                      ),
                     ),
                     Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: Text('Documento: 12345695',  style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.w500),),
+                      child: Text(
+                        'Documento: ${_.user.document}',
+                        style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.w500),
+                      ),
                     ),
                     Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: Text('Id:  B1-52364', style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.w500),),
+                      child: Text(
+                        'Id:  ${_.user.id}',
+                        style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.w500),
+                      ),
                     )
                   ],
                 )
@@ -56,28 +71,31 @@ class  ProfilePage extends StatelessWidget {
           ),
           Divider(),
           Padding(
-            padding:  EdgeInsets.only(left: 18, top: 8.0),
-            child: Text('Direccion: Lorem ipsum dolor sit amet ', style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),),
+            padding: EdgeInsets.only(left: 18, top: 8.0),
+            child: Text(
+              'Descripcion: ${_.user.description}',
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
+            ),
           ),
           Padding(
-            padding:  EdgeInsets.only(left: 18, top: 8.0),
-            child: Text('Telefono: 1234567',style: TextStyle(fontSize:18.0, fontWeight: FontWeight.w500),),
+            padding: EdgeInsets.only(left: 18, top: 8.0),
+            child: Text(
+              'Telefono: 1234567',
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
+            ),
           ),
           Padding(
-            padding:   EdgeInsets.only(left: 18, top: 8.0),
-            child: Text('Fecha de ingreso: 25/12/2020',style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),),
+            padding: EdgeInsets.only(left: 18, top: 8.0),
+            child: Text(
+              'Fecha de ingreso: 25/12/2020',
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
+            ),
           ),
-
-
-
-
-
         ],
-      ),
+      ),),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-
-        onPressed: (){
+        onPressed: () {
           Get.to(CreateAccountPage());
         },
       ),
